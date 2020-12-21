5fc6a3258a133329f59901fa13cb28e35b3e3330
chenweiyan <chanweiyan007@gmail.com>
2020-12-21 18:44:50 +0800

    Readonly, Partial, Pick and Record

--- docs/demos/ts-base/src/syntax/20.advanced-3.ts
+++ docs/demos/ts-base/src/syntax/20.advanced-3.ts
@@ -1,11 +1,19 @@
-interface Obj {
+// ts 的映射类型
+// 从旧类型生成新类型
+//
+interface Obj33 {
     a: string;
     b: number;
 }
-type ReadonlyObj = Readonly<Obj>
 
-type PartialObj = Partial<Obj>
+// 成员的属性变成只读
+// 同态
+type ReadonlyObj = Readonly<Obj33>;
+type PartialObj = Partial<Obj33>;
+type PickObj = Pick<Obj33, "a" | "b">;
 
-type PickObj = Pick<Obj, 'a' | 'b'>
-
-type RecordObj = Record<'x' | 'y', Obj>
+// 创建新的属性
+// "x" | "y" 预定义的新的属性
+// Obj33 已知的类型
+//
+type RecordObj = Record<"x" | "y", Obj33>;
