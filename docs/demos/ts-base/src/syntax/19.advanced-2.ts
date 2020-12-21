5194611e246cc8989a3d8b78b4aabef6c12b0dad
chenweiyan <chanweiyan007@gmail.com>
2020-12-21 18:32:19 +0800

    K extends keyof T, T[K][]

--- docs/demos/ts-base/src/syntax/19.advanced-2.ts
+++ docs/demos/ts-base/src/syntax/19.advanced-2.ts
@@ -1,4 +1,5 @@
-let obj = {
+// ts 中的索引类型
+let obj12 = {
     a: 1,
     b: 2,
     c: 3
@@ -7,20 +8,29 @@ let obj = {
 // function getValues(obj: any, keys: string[]) {
 //     return keys.map(key => obj[key])
 // }
+
+// 索引查询操作符
+// keyof T ，表示类型T的所有公共属性字面量的联合类型
+//
 function getValues<T, K extends keyof T>(obj: T, keys: K[]): T[K][] {
     return keys.map(key => obj[key])
 }
-console.log(getValues(obj, ['a', 'b']))
+console.log(getValues(obj12, ['a', 'b']))
 // console.log(getValues(obj, ['d', 'e']))
 
-// keyof T
 interface Obj {
     a: number;
     b: string;
 }
-let key: keyof Obj
 
+// 查询操作符
+// keyof T
+let key: keyof Obj
+// let key: "a" | "b";
+
+// 索引访问操作符
 // T[K]
 let value: Obj['a']
 
+// 泛型约束
 // T extends U
